42eb71ed95c5fe610d90e5de0cfb4c17a797b3c6
jojomak13 <jojomak350@gmail.com>
2021-01-09 02:59:28 +0200

    configure delete route with its tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,39 @@
+import {
+  auth,
+  BadRequestError,
+  currentUser,
+  NotFoundError,
+  OrderStatus,
+} from '@jmtickt/common';
 import { Router, Request, Response } from 'express';
+import Order from '../models/Order';
 
 const router = Router();
 
-router.delete('/:orderId', async (req: Request, res: Response) => {
-  res.send('delete route');
-});
+router.delete(
+  '/:orderId',
+  auth,
+  currentUser,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.user!.id) {
+      throw new BadRequestError('not authorized', 401);
+    }
+
+    order.status = OrderStatus.Canelled;
+    await order.save();
+
+    // publish an event that order cancelled
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as DeleteOrderRouter };
