4a08ff896e19d85cbbf884b3357a8242e777e110
jojomak13 <jojomak350@gmail.com>
2021-01-09 02:17:12 +0200

    configure show route with its tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import app from '../../app';
+import Order from '../../models/Order';
+import Ticket from '../../models/Ticket';
+
+it('returns an error 401 if another user try to fetch another order', async () => {
+  const ticket = Ticket.build({ title: 'ticket title', price: 45 });
+  await ticket.save();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
+
+it('returns an order', async () => {
+  const ticket = Ticket.build({ title: 'ticket title', price: 45 });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,33 @@
+import {
+  auth,
+  BadRequestError,
+  currentUser,
+  NotFoundError,
+} from '@jmtickt/common';
 import { Router, Request, Response } from 'express';
+import Order from '../models/Order';
 
 const router = Router();
 
-router.get('/:orderId', async (req: Request, res: Response) => {
-  res.send('show route');
-});
+router.get(
+  '/:orderId',
+  auth,
+  currentUser,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.user!.id) {
+      throw new BadRequestError('not authorized', 401);
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as ShowOrderRouter };
