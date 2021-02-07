ef47139850824db3e1287d528d31d982cc5c41f3
jojomak13 <jojomak350@gmail.com>
2021-02-07 03:34:32 +0200

    configure payments service files

---
+++ payments/index.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import app from './src/app';
+import { natsWrapper } from './src/natsWrapper';
+
+const start = async () => {
+  const envKeys = [
+    'JWT_KEY',
+    'MONGO_URI',
+    'NATS_CLUSTER_ID',
+    'NATS_CLIENT_ID',
+    'NATS_URL',
+  ];
+
+  for (let key of envKeys) {
+    if (!process.env[key]) {
+      throw new Error(`[${key}] not found`);
+    }
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    console.log('[info] nats connection open.');
+
+    natsWrapper.client.on('colse', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // Start listeners
+    // some listeners here
+
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log('[info] DB connection open.');
+  } catch (err) {
+    console.log(err);
+  }
+
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`[Tickets Service] Running on port ${port}`);
+  });
+};
+
+start();
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,60 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+jest.mock('../natsWrapper');
+
+let mongo: MongoMemoryServer;
+
+// Configure MongoMemoryServer
+// before starting
+beforeAll(async () => {
+  process.env.JWT_KEY = 'jojo';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Create jwt token
+  const token = jwt.sign(
+    { id: new mongoose.Types.ObjectId().toHexString(), email: 'jojo@test.com' },
+    process.env.JWT_KEY!
+  );
+
+  // Build session Object and convert it to JSON
+  const session = JSON.stringify({ jwt: token });
+
+  // Convert to base64
+  const base64Session = Buffer.from(session).toString('base64');
+
+  return [`express:sess=${base64Session}`];
+};
