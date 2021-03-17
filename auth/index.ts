064c23429446b33348f7951890f2d2af56205037
jojomak13 <jojomak350@gmail.com>
2021-03-17 13:45:35 +0200

    enhance env keys check

--- auth/index.ts
+++ auth/index.ts
@@ -2,16 +2,14 @@ import mongoose from 'mongoose';
 import app from './src/app';
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('[JWT_KEY] not found');
-  }
+  const keys = ['JWT_KEY', 'MONGO_URI'];
 
-  if (!process.env.MONGO_URI) {
-    throw new Error('[MONGO_URI] not found');
+  for (let key of keys) {
+    if (!process.env[key]) throw new Error(`[${key}] not found`);
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
